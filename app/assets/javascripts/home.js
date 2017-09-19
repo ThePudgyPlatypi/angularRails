3835ffdde0abd059dfe4964452ab20d920175f96
ThePudgyPlatypi <cstehm@comcast.net>
2017-09-19 13:52:43 -0400

    f*** TDD on WSL

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,16 +1,50 @@
 // dont forget to remove the comments at the top or JS files wont load properly.
 
-var app = angular.module('receta', ['templates, 'ngRoute', 'controllers'])
+var app = angular.module('receta', ['templates' , 'ngRoute', 'controllers']);
 
 app.config(['$routeProvider', function($routeProvider){
 	$routeProvider.when('/', {
 		templateUrl: "index.html",
 		controller: "RecipesController"
 	})
-}])
+}]);
 
-var controllers = angular.module('controllers', [])
+recipes = [
+  {
+    id: 1,
+    name: 'Baked Potato w/ Cheese'
+  },
+  {
+    id: 2,
+    name: 'Garlic Mashed Potatoes'
+  },
+  {
+    id: 3,
+    name: 'Potatoes Au Gratin'
+  },
+  {
+    id: 4,
+    name: 'Baked Brussel Sprouts'
+  },
+];
 
-controllers.controller("RecipesController", ['$scope', function($scope) {
-	$scope
-}])
+var controllers = angular.module('controllers', []);
+
+controllers.controller("RecipesController", ['$scope', 
+	'$routeParams', 
+	'$location', 
+	function($scope, $routeParams, $location) {
+	 	var keywords;
+		$scope.search = function(keywords) {
+			return $location.path("/").search('keywords', keywords);
+		};
+
+		if ($routeParams.keywords) {
+			keywords = $routeParams.keywords.toLowerCase();
+			return $scope.recipes = recipes.filter(function(recipe) {
+		  		return recipe.name.toLowerCase().indexOf(keywords) !== -1;
+			});
+		} else {
+			return $scope.recipes = [];
+		}
+}]);
